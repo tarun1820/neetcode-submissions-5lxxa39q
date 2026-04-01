979ed396c9d6ab3f706f8d4feeb5994414abb035
Edupuganti Tarun Venkata Sai <79135830+tarun1820@users.noreply.github.com>
2026-04-01 16:44:56 +0530

    Add: longest-repeating-substring-with-replacement - submission-3

---
+++ Data Structures & Algorithms/longest-repeating-substring-with-replacement/submission-3.java
@@ -0,0 +1,38 @@
+class Solution {
+    public int characterReplacement(String s, int k) {
+        int i=0;
+        int j=1;
+        int len = s.length();
+        int[] charFreq = new int[26];
+        charFreq[s.charAt(i)-'A']++;
+        int maxFreq = 1;
+        char maxFreqChar = s.charAt(0);
+        int ans = 1;
+        while(j<len && i<len){
+            System.out.println("i = " + i +"char[i] = "+ s.charAt(i));
+            System.out.println("j = " + j +"char[j] = "+ s.charAt(j));
+            int currLen = j-i+1;
+            charFreq[s.charAt(j)-'A']++;
+            if( maxFreq < charFreq[s.charAt(j)-'A']){
+                maxFreqChar =  s.charAt(j);
+                maxFreq = charFreq[s.charAt(j)-'A'];
+            }
+            while(currLen - maxFreq > k){
+                charFreq[s.charAt(i)-'A']--;
+                if(maxFreqChar == s.charAt(i)){
+                    maxFreq=0;
+                    for(int p=0;p<26;p++){
+                        maxFreq = Math.max(charFreq[p],maxFreq);
+                    }
+                }
+                i++;
+                currLen--;
+            }
+            if(currLen - maxFreq <=k){
+                ans = Math.max(ans,currLen);
+                j++;
+            }
+        }
+        return ans;
+    }
+}
